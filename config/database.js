5d69d0a1e283888a45e4593aadb006ab602892f9
sudeeepaa <sudeepasanthanam2004@gmail.com>
2025-08-24 20:25:50 +0530

    Initial commit

--- config/database.js
+++ config/database.js
@@ -1,15 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '123456',
-  database: 'fsd_labex9',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
 });
 
-const promisePool = pool.promise();
-
-module.exports = promisePool;
+module.exports = pool;
